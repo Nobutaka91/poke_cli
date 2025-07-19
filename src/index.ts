b378be90cd8a1c0c7ef5a0ed276a2c4f0b46d175
NobutakaSuzuki <suzuki_n@opening-line.co.jp>
2025-07-19 23:54:24 +0900

    TypeScript開発環境構築完了

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+console.log("=== Pokemon CLI ===");
+
+const pokemonName = "Pikachu";
+const pokemonType = "Electric";
+
+console.log(`Name: ${pokemonName}`);
+console.log(`Type: ${pokemonType}`);
+
+const maxHp = 100;
+const hp = 100;
+
+
+console.log(`HP: ${hp} / ${maxHp}`);
